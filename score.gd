df3c2d8d9078e06d3f5288e1dd7faabeaade0dd7
anastasiya-zhyrkevich <anastasiya.zhyrkevich@yandex.ru>
2020-04-02 23:40:49 +0300

    Tasks

--- score.gd
+++ score.gd
@@ -3,17 +3,7 @@ extends HBoxContainer
 var player_labels = {}
 
 func _process(_delta):
-	var rocks_left = $"../Rocks".get_child_count()
-	if rocks_left == 0:
-		var winner_name = ""
-		var winner_score = 0
-		for p in player_labels:
-			if player_labels[p].score > winner_score:
-				winner_score = player_labels[p].score
-				winner_name = player_labels[p].name
-
-		$"../Winner".set_text("THE WINNER IS:\n" + winner_name)
-		$"../Winner".show()
+	pass
 
 
 sync func increase_score(for_who):
@@ -40,6 +30,8 @@ func add_player(id, new_player_name):
 func _ready():
 	$"../Winner".hide()
 	set_process(true)
+	
+	
 
 
 func _on_exit_game_pressed():
